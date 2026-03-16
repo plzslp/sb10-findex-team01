710b5b8bfaa43a782b65f694b51fd4dfb7e48868
Nooroong <sjiyeon759@gmail.com>
2026-03-16 16:50:35 +0900

    feat: SyncJob Service, Controller 구현
    --- 본문 (선택 사항, 상세 설명이 필요한 경우) ---
    - SyncJobService
      - DB에 저장된 지수 정보를 바탕으로 Open API로부터 값을 받아와 값을 연동합니다.
      - 지수 이름, 채용종목 수, 기준 일자를 바탕으로 Open API에서 지수 정보를 받아옵니다.
      - 연동 히스토리를 DB에 저장합니다.
    - IndexInfo
      - employedItemsCount: Open API에서 주는 값의 크기가 15자리라서 타입을 Long으로 변경하였습니다.(Open API 문서에서 beginEpyItmsCnt, endEpyItmsCnt 참고)
    - BusinessLogicException
      - 커스텀 예외 코드 작성
    
    --- 꼬리말 (선택 사항, 터이슈 번호 연결) ---
    feat: #9

--- src/main/java/com/sprint/project/findex/entity/IndexInfo.java
+++ src/main/java/com/sprint/project/findex/entity/IndexInfo.java
@@ -2,12 +2,14 @@ package com.sprint.project.findex.entity;
 
 import com.sprint.project.findex.dto.IndexInfoDto;
 import com.sprint.project.findex.entity.base.BaseEntity;
+import com.sprint.project.findex.indexinfo.external.dto.StockMarketIndexAPIResponse;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,7 +27,7 @@ public class IndexInfo extends BaseEntity {
   private String indexName;
 
   @Column(nullable = false)
-  private Integer employedItemsCount;
+  private Long employedItemsCount;
 
   @Column(nullable = false)
   private LocalDate basePointInTime;
@@ -45,7 +47,7 @@ public class IndexInfo extends BaseEntity {
   private DeletedStatus isDeleted;
 
   @Builder
-  public IndexInfo(String indexClassification, String indexName, Integer employedItemsCount,
+  public IndexInfo(String indexClassification, String indexName, Long employedItemsCount,
       LocalDate basePointInTime, Double baseIndex, SourceType sourceType, boolean favorite,
       DeletedStatus isDeleted) {
     this.indexClassification = indexClassification;
@@ -60,5 +62,28 @@ public class IndexInfo extends BaseEntity {
 
   // todo
   public void update(IndexInfoDto dto) {
+  }
+
+  public void updateByOpenAPI(StockMarketIndexAPIResponse.IndexDto indexDto) {
+    this.indexClassification = indexDto.indexClassification();
+    this.indexName = indexDto.indexName();
+    this.employedItemsCount = indexDto.employedItemsCount(); // todo: 타입이 다른데 짤릴 것 같음(Long to Integer)
+    this.basePointInTime = LocalDate.parse(indexDto.basePointInTime(),
+        DateTimeFormatter.ofPattern("yyyyMMdd"));
+    this.sourceType = SourceType.OPEN_API;
+  }
+
+  @Override
+  public String toString() {
+    return "IndexInfo{" +
+        "indexClassification='" + indexClassification + '\'' +
+        ", indexName='" + indexName + '\'' +
+        ", employedItemsCount=" + employedItemsCount +
+        ", basePointInTime=" + basePointInTime +
+        ", baseIndex=" + baseIndex +
+        ", sourceType=" + sourceType +
+        ", favorite=" + favorite +
+        ", isDeleted=" + isDeleted +
+        '}';
   }
 }
---
+++ src/main/java/com/sprint/project/findex/service/SyncJobService.java
@@ -0,0 +1,90 @@
+package com.sprint.project.findex.service;
+
+import com.sprint.project.findex.dto.SyncJobDto;
+import com.sprint.project.findex.entity.IndexInfo;
+import com.sprint.project.findex.entity.SyncJob;
+import com.sprint.project.findex.global.entity.JobType;
+import com.sprint.project.findex.global.entity.ResultType;
+import com.sprint.project.findex.global.exception.BusinessLogicException;
+import com.sprint.project.findex.global.exception.ExceptionCode;
+import com.sprint.project.findex.indexinfo.external.dto.StockMarketIndexAPIResponse;
+import com.sprint.project.findex.mapper.SyncJobMapper;
+import com.sprint.project.findex.repository.IndexInfoRepository;
+import com.sprint.project.findex.repository.SyncJobRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import java.time.Duration;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class SyncJobService {
+
+  private final IndexInfoRepository indexInfoRepository;
+  private final SyncJobRepository syncJobRepository;
+  private final WebClient webClient;
+
+  private final SyncJobMapper syncJobMapper;
+
+  @Value("${LOCAL_INDEX_API_KEY}")
+  private String apiKey;
+
+  // DB에 저장된 지수 정보를 바탕으로 실제 값을 Open API로부터 조회하고 기록합니다.
+  public List<SyncJobDto> syncIndexInfos(HttpServletRequest request) {
+    // DB로부터 지수 정보를 불러온다.
+    List<IndexInfo> indexInfos = indexInfoRepository.findAll();
+
+    List<SyncJobDto> syncJobDtos = new ArrayList<>(); // 컨트롤러 응답
+    String requestIpAddr = request.getRemoteAddr();
+
+    // 모든 지수 데이터에 대해 작업을 반복한다.
+    for (IndexInfo indexInfo : indexInfos) {
+      // 지수 정보 가져오기.
+
+      ResultType resultType = ResultType.FAIL;
+
+      try {
+        // todo: 아래의 WebClient 코드는 임의로 작성함. 나중에 분리 및 교체할 예정.
+        StockMarketIndexAPIResponse apiResponse = webClient.get()
+            .uri(urlBuilder -> urlBuilder
+                .queryParam("serviceKey", apiKey)
+                .queryParam("resultType", "json")
+                .queryParam("idxNm", indexInfo.getIndexName())
+                .queryParam("beginEpyItmsCnt", indexInfo.getEmployedItemsCount())
+                .queryParam("basDt",
+                    indexInfo.getBasePointInTime().format(DateTimeFormatter.ofPattern("yyyyMMdd")))
+                .build()
+            )
+            .retrieve() // response body를 추출
+            .bodyToMono(StockMarketIndexAPIResponse.class)
+            .block(Duration.ofSeconds(5));
+
+        // 지수 데이터 갱신
+        if (apiResponse.response().header().resultCode().equals("00")
+            && !apiResponse.response().bodyDto().items().item()
+            .isEmpty()) {
+          indexInfo.updateByOpenAPI(apiResponse.response().bodyDto().items().item().get(0)); // 갱신
+          resultType = ResultType.SUCCESS;
+        }
+      } catch (Exception e) {
+        new BusinessLogicException(ExceptionCode.OPEN_API_REQUEST_FAILED, e.getMessage());
+      } finally {
+        // SyncJob 히스토리 등록
+        SyncJob syncJob = new SyncJob(indexInfo, JobType.INDEX_INFO, null, requestIpAddr,
+            resultType);
+        syncJobRepository.save(syncJob);
+
+        syncJobDtos.add(syncJobMapper.toDto(syncJob));
+      }
+    }
+
+    return syncJobDtos;
+  }
+}
