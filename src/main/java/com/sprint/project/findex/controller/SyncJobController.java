868b7ac522ebf1df88a4612354b5b23c3253cecf
Nooroong <sjiyeon759@gmail.com>
2026-03-17 11:03:23 +0900

    feat: SyncJobController 작성
    --- 본문 (선택 사항, 상세 설명이 필요한 경우) ---
    - /sync-jobs/index-data 엔드포인트 추가
    
    --- 꼬리말 (선택 사항, 이슈 번호 연결) ---
    feat: #12

--- src/main/java/com/sprint/project/findex/controller/SyncJobController.java
+++ src/main/java/com/sprint/project/findex/controller/SyncJobController.java
@@ -1,12 +1,15 @@
 package com.sprint.project.findex.controller;
 
 import com.sprint.project.findex.dto.SyncJobDto;
+import com.sprint.project.findex.dto.syncjob.IndexDataSyncRequest;
 import com.sprint.project.findex.service.SyncJobService;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +23,13 @@ public class SyncJobController {
   @PostMapping("/index-infos")
   public ResponseEntity<List<SyncJobDto>> syndIndexInfo(HttpServletRequest request) {
     return ResponseEntity.ok(syncJobService.syncIndexInfos(request));
+  }
+
+  @PostMapping("/index-data")
+  public ResponseEntity<?> syncIndexData(
+      @Valid @RequestBody IndexDataSyncRequest indexDataSyncRequest,
+      HttpServletRequest request) {
+    return ResponseEntity.ok(syncJobService.syncIndexData(indexDataSyncRequest, request));
   }
 
 }
